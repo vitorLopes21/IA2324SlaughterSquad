19e433c1244358505de0e8d2b0195b0e28ee48e3
João Antunes <8210474@estg.ipp.pt>
2024-06-04 19:30:25 +0100

    Finished IntelligentRobot

---
+++ SciptsPython/RunH2O.py
@@ -0,0 +1,6 @@
+import h2o
+h2o.init()
+
+# Run this with 
+# & C:/Users/Utilizador/AppData/Local/Programs/Python/Python39/python.exe 
+# c:/Users/Utilizador/Desktop/Cenas_do_Instituto/Ano3/IA/IA2324SlaughterSquad/SciptsPython/RunH2O.py
